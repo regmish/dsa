6df9afcb14f2dbfa3f4d59ff83180cf24424905c
Shankar <shankarregmi@gmail.com>
2021-04-04 15:36:16 +0200

    heap poll

--- ds/queue/heap.js
+++ ds/queue/heap.js
@@ -29,12 +29,13 @@ class BinaryHeap {
   poll() {
     const top = this.peek();
 
-    if(this.size === 1) {
-      return this[items].pop();
-    } else {
+    if (this.size > 1) {
       this[items][0] = this[items].pop();
       this[heapifyDown]();
+
       return top;
+    } else {
+      return this[items].pop();
     }
   }
 
@@ -59,9 +60,9 @@ class BinaryHeap {
   [heapifyUp]() {
     let currIdx = this.size - 1;
 
-    while(currIdx > 0) {
+    while (currIdx > 0) {
       let parentIndex = this[getparentIdx](currIdx);
-      if(this[compare](parentIndex, currIdx)) {
+      if (this[compare](parentIndex, currIdx)) {
         this[swap](parentIndex, currIdx);
         currIdx = parentIndex;
       } else {
@@ -73,16 +74,16 @@ class BinaryHeap {
   [heapifyDown]() {
     let currIdx = 0;
 
-    while(this[hasLeft](currIdx)) {
+    while (this[hasLeft](currIdx)) {
       let replaceWith = this[getLeftChildIdx](currIdx);
 
-      if(this[hasRight](currIdx)) {
-        if(this[compare](replaceWith, this[getRightChildIdx](currIdx))) {
+      if (this[hasRight](currIdx)) {
+        if (this[compare](replaceWith, this[getRightChildIdx](currIdx))) {
           replaceWith = this[getRightChildIdx](currIdx)
         }
       }
 
-      if(this[compare](currIdx, replaceWith)) {
+      if (this[compare](currIdx, replaceWith)) {
         this[swap](currIdx, replaceWith);
         currIdx = replaceWith;
       } else {
@@ -128,16 +129,15 @@ class BinaryHeap {
 
 if (require.main === module) {
   const q = new BinaryHeap('max');
-  q.offer(35)
-  q.offer(20)
-  q.offer(30)
-  q.offer(40)
-  q.offer(10)
-  q.offer(25)
+  q.offer(35);
+  q.offer(20);
+  q.offer(30);
+  q.offer(40);
+  q.offer(10);
+  q.offer(25);
   q.offer(32);
-  q.print();
-  // console.log(q.size);
-  for (let i = 0; i < 7; i++) {
-    console.log(q.poll());
-  }
+
+ while(q.size) {
+   console.log(q.poll());
+ }
 }
